1e90e23c8b5cb641d4cb622aa0921d8010efd53f
Alena Gilevskaya <alena@gilevskaya.com>
2020-02-22 10:08:44 -0800

    better go

--- index.go
+++ index.go
@@ -13,38 +13,45 @@ const fileName = "map.db"
 func main() {
 	input := bufio.NewScanner(os.Stdin)
 	fmt.Print("> ")
+
 	for input.Scan() {
 		input := strings.Fields(input.Text())
-		processInput(input)
+		if err := processInput(input); err != nil {
+			fmt.Println("ERROR HAPPENED:", err.Error())
+		}
 		fmt.Print("> ")
 	}
 }
 
-func processInput(words []string) {
-	if len(words) == 2 && (words[0] == "get" || words[0] == "set") {
-		action := words[0]
-		arg := words[1]
-
-		switch action {
-		case "set":
-			pair := strings.Split(arg, "=")
-			if len(pair) == 2 {
-				addRecord(pair[0], pair[1])
-				return
-			}
-		case "get":
-			value := findRecord(arg)
-			if value != "" {
-				fmt.Println(arg, " => ", value)
-				return
-			}
-		}
+func processInput(words []string) error {
+	if len(words) != 2 {
+		return fmt.Errorf("invalid number of arguments")
 	}
-	fmt.Println("hmm...")
+	action, arg := words[0], words[1]
+	switch action {
+	case "set":
+		pair := strings.Split(arg, "=")
+		if len(pair) != 2 {
+			return fmt.Errorf("invalid syntax for set")
+		}
+		addRecord(pair[0], pair[1])
+	case "get":
+		value, err := findRecord(arg)
+		if err != nil {
+			return fmt.Errorf("failed to find record: %s", err.Error())
+		}
+		if value != nil {
+			fmt.Println("Your value was:", *value)
+		} else {
+			fmt.Println("Unable to find your value")
+		}
+
+	}
+	return nil
 }
 
 func getFileRead(fileName string) *os.File {
-	file, err := os.Open(fileName)
+	file, err := os.OpenFile(fileName, os.O_CREATE, 0644)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -52,33 +59,35 @@ func getFileRead(fileName string) *os.File {
 }
 
 func getFileWrite(fileName string) *os.File {
-	file, err := os.OpenFile(fileName, os.O_APPEND|os.O_WRONLY, 0644)
+	file, err := os.OpenFile(fileName, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
 	if err != nil {
 		log.Fatal(err)
 	}
 	return file
 }
 
-func findRecord(key string) (value string) {
+func findRecord(key string) (*string, error) {
 	r := getFileRead(fileName)
 	defer r.Close()
 
 	scanner := bufio.NewScanner(r)
+	var res *string
 	for scanner.Scan() {
 		parts := strings.Split(scanner.Text(), "=")
+		if len(parts) != 2 {
+			continue
+		}
 		if key == parts[0] {
-			return parts[1]
+			res = &parts[1]
 		}
 	}
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
-	}
-	return ""
+	return res, scanner.Err()
 }
 
 func addRecord(key string, value string) {
 	w := getFileWrite(fileName)
 	defer w.Close()
+
 	// TODO: handle re-assignment
-	fmt.Fprintln(w, key+"="+value)
+	fmt.Fprintf(w, "%s=%s\n", key, value)
 }
